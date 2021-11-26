612b539473555a2c80e30bc73306e3065e859896
1NT18IS177 <1nt18is177.upanshu@nmit.ac.in>
2021-11-26 15:30:42 +0530

    2nd Program

---
+++ app/src/main/java/com/example/addition177/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.addition177;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText num1, num2;
+    TextView sum;
+    Button addition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = (EditText) findViewById(R.id.num1);
+        num2 = (EditText) findViewById(R.id.num2);
+        sum = (TextView) findViewById(R.id.sum);
+        addition = (Button) findViewById(R.id.add);
+
+        addition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(num1.getText().toString());
+                int b = Integer.parseInt(num2.getText().toString());
+                int Sum = a + b;
+                sum.setText(Integer.toString(Sum));
+            }
+        });
+    }
+
+}
+
